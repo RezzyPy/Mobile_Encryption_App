ed819a263595aa703df736242f2f0adb3c694956
Peewee420 <162775922+Peewee420@users.noreply.github.com>
2024-03-16 20:23:45 -0700

    Update ChatScreen.js

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ChatScreen = ({ route }) => {
+  const { friend } = route.params;
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Chat with {friend}</Text>
+    </View>
+  );
+};
+
+export default ChatScreen;
 
